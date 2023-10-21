2b4a31253cc9949dca43ad37da9e4a5f1f0695ec
szczepaniak2002 <101816148+szczepaniak2002@users.noreply.github.com>
2023-10-21 12:05:07 +0200

    Add files via upload

---
+++ wyciaganie_artykulow.py
@@ -0,0 +1,46 @@
+
+import pandas as pd
+import re
+def main():
+
+    orzeczenia = pd.read_csv("orzeczenia2.csv")
+
+    pd.set_option('display.max_colwidth', None)
+    numery_artykulow = []
+    numery_paragrafow = []
+    szukany_wzor1 = r"art\. (\d+) § (\d+)"
+    szukany_wzor2 = r"art\. (\d+)(?![§\d])"
+
+    for i in range(orzeczenia.size):
+        orzeczenie = str(orzeczenia.iloc[i])
+
+        dopasowania1 = re.findall(szukany_wzor1, orzeczenie)
+        dopasowania2 = re.findall(szukany_wzor2, orzeczenie)
+        for dopasowanie1 in dopasowania1:
+            numery_artykulow.append(dopasowanie1[0])
+            numery_paragrafow.append(dopasowanie1[1])
+
+        for dopasowanie2 in dopasowania2:
+            numery_artykulow.append(dopasowanie2[0])
+            numery_paragrafow.append(1)
+
+
+
+
+    spis_artykulow_duplikaty = pd.DataFrame({
+        'Artykul': numery_artykulow,
+        'Paragraf': numery_paragrafow
+    })
+    wystepujace_artykuly_bez_duplikatow = spis_artykulow_duplikaty.drop_duplicates()
+    wystepujace_artykuly_bez_duplikatow_posortowane= wystepujace_artykuly_bez_duplikatow.sort_values(['Artykul', 'Paragraf'])
+    print(wystepujace_artykuly_bez_duplikatow_posortowane.head(100))
+
+
+
+
+
+
+
+
+if __name__ =="__main__":
+    main()
